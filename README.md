c059b8fc7d394ff41500ff2273753e052201ebd3
GeraMG1105 <160290048+GeraMG1105@users.noreply.github.com>
2024-02-20 00:40:42 +0000

    version 0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hola-Mundo-3G
+
+##Practica 1 _ Hola mundo
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+    
+// namespace name
+
+
+int main(int argc, char const *argv[])
+{
+    
+cout << " Hola mundo"<<"23110200/n";
+
+
+    return 0;
+}
